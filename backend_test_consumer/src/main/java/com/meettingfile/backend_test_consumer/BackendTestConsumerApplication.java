0b7aab19615785cd9ab86b26092a8bfcd38c887e
chuck.chen@ilinklink.com <chuck.chen@ilinklink.com>
2020-04-21 17:37:10 +0800

    prefer-ip-address: true   instance-id: 用户服务:${host}:${server.port} #将显示在status栏   ip-address: ${host}   # 服务的ip地址   status-page-url: http://${host}:${server.port}/swagger-ui.html # 配置swager

--- backend_test_consumer/src/main/java/com/meettingfile/backend_test_consumer/BackendTestConsumerApplication.java
+++ backend_test_consumer/src/main/java/com/meettingfile/backend_test_consumer/BackendTestConsumerApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
+import io.swagger.annotations.ApiOperation;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
@@ -18,6 +23,11 @@ public class BackendTestConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendTestConsumerApplication.class, args);
+	}
+
+	@Bean
+	public Docket swaggerSpringMvcPlugin() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)).build();
 	}
 
 }
--- backend_test_provider/src/main/java/com/meettingfilm/backend_test_provider/BackendTestProviderApplication.java
+++ backend_test_provider/src/main/java/com/meettingfilm/backend_test_provider/BackendTestProviderApplication.java
@@ -7,7 +7,12 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
 
+import io.swagger.annotations.ApiOperation;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableCircuitBreaker
@@ -21,6 +26,11 @@ public class BackendTestProviderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendTestProviderApplication.class, args);
+	}
+
+	@Bean
+	public Docket swaggerSpringMvcPlugin() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)).build();
 	}
 
 }
