4f561931722301200ff323f519b48c52977aba40
chuck.chen@ilinklink.com <chuck.chen@ilinklink.com>
2020-07-09 10:53:41 +0800

    请求耗时记录

--- backend_gateway/src/main/java/com/meettingfilm/backend/backend_gateway/filters/LogGlobalFilter.java
+++ backend_gateway/src/main/java/com/meettingfilm/backend/backend_gateway/filters/LogGlobalFilter.java
@@ -5,6 +5,12 @@ import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.web.server.ServerWebExchange;
+
+import java.net.InetSocketAddress;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
+
 import reactor.core.publisher.Mono;
 
 /**
@@ -15,6 +21,8 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class LogGlobalFilter implements GlobalFilter, Ordered {
 
+  private SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
   /**
   * @Description: 业务处理逻辑
   * @Param: [exchange, chain]
@@ -24,10 +32,35 @@ public class LogGlobalFilter implements GlobalFilter, Ordered {
   @Override
   public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
     long beginTime = System.currentTimeMillis();
-    log.info("filter start ----> beginTime:{}", beginTime);
+    //log.info("filter start ----> beginTime:{}", beginTime);
     return chain.filter(exchange).then(Mono.fromRunnable(()->{
       long endTime = System.currentTimeMillis();
-      log.info("filter end ----> endTime: [{}], cost: [{}] ms",endTime , endTime-beginTime);
+
+      String uri= null;
+      try {
+        uri = exchange.getRequest().getURI().getPath();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+      InetSocketAddress remoteAddress = null;
+      try {
+        remoteAddress = exchange.getRequest().getRemoteAddress();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+      String clientIp = null;
+      try {
+        clientIp = Objects.requireNonNull(remoteAddress).getAddress().getHostAddress();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+      String time= null;
+      try {
+        time = simpleDateFormat.format(new Date(beginTime));
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+      log.info("请求耗时记录： ----> uri: [{}] ， 时间: [{}] , 耗时: [{}] 毫秒 ,ip地址:[{}]",uri,time , endTime-beginTime,clientIp);
     }));
   }
 
